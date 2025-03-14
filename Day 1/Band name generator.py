3099f92790072b64b9a8d621ff8df630d24a401d
Suhaib Nayyar Mallick <135153061+snmallick2401@users.noreply.github.com>
2025-03-14 17:26:27 +0530

    Add files via upload

---
+++ Day 1/Band name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("what's the name of the city you grew up in ")
+pet = input("what if the name of your pet ")
+print("Your band name could be" , city , pet)
