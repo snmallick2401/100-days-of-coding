a15752a61fb3764afa2872e005ffadc11566a078
Suhaib Nayyar Mallick <135153061+snmallick2401@users.noreply.github.com>
2025-03-17 06:12:29 +0530

    Delete day 2 directory

--- day 2/tip calculator.py
+++
@@ -1,8 +0,0 @@
-print("welcome to the tip calculator")
-total_bill = float(input("What is the total bill "))
-percentage = float(input("how much tip you want to give? 10%, 12%, or 15%? "))
-split = int(input("How many people to split the bill? "))
-tip_per_person = (total_bill * (percentage / 100)) / split
-share_per_person = total_bill / split
-total_per_person = share_per_person + tip_per_person
-print("Each person should pay: ₹" + str(round(total_per_person, 2)))
